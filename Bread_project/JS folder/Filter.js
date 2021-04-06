fa1e89328789f11b9597e0bf69540ca7ef0031c2
Nadav Yair <nadav.isshar@gmail.com>
2021-04-06 10:47:49 +0300

    Add files via upload

---
+++ Bread_project/JS folder/Filter.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+import classes from '../Filter/Filter.module.css';
+
+export default class Filter extends Component {
+    render() {
+        return (
+            <div className={classes.Filter}>
+                <div className={classes.filterResult}>{this.props.count} Prodacts</div>
+                <div className={classes.filterSort}> Order{" "} 
+                <select value={this.props.sort} onChange={()=>this.props.sortItems}>
+                <option value='Latest'>Latest</option>
+                <option value='Lowest'>Lowest To Highest</option>
+                <option value='Highest'>Highest To Lowest</option>
+                    </select>
+                    </div>
+                <div className='filterFlourType'> Flour Type{" "}
+                <select value={this.props.type} onChange={this.props.filterItems}>
+                    <option value='Wheat'>Wheat</option>
+                    <option value='Simplify'>Simplify</option>
+                    <option value='Spelt'>Spelt</option>
+                    <option value='Grain'>Grain</option>
+                    </select>
+                    </div>
+            </div>
+        )
+    }
+}
