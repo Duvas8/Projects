2c7018ed58206d9b5815d37dd7c07b3186809fa0
Nadav Yair <nadav.isshar@gmail.com>
2021-04-20 15:33:16 +0300

    Update Checkout.js

--- Bread_project/JS folder/Checkout.js
+++ Bread_project/JS folder/Checkout.js
@@ -1,45 +1,118 @@
 import React, { Component } from 'react'
-import  '../Cart/cart.css';
+import  './Checkout.css';
 
+import formatCurrency from '../../../util'
 
 
 
 export default class Checkout extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            name: "",
+            phoneNumber:"",
+            peckupPoint:""
+         }
+        };
+
+    handleInput = (e) => {
+        this.setState({[e.target.name]: e.target.value});
+        console.log(e.target.value);
+       };
+
+       createOrder = (e) => {
+        e.preventDefault();
+        const order = {
+            name: this.state.name,
+            phoneNumber: this.state.phoneNumber,
+            peckupPoint: this.state.peckupPoint,
+            cartItems: this.props.cartItems,
+        };
+       
+        this.props.createOrder(order)
+    };
+
     render() {
+
+        const {cartItems} = this.props;
         return (
             <div className="checkout_container">
+                <div className="checkout_items_container">
+                    <div className="checkout_items_info">
+                        <div>Name</div>
+                        <div>Amount</div>
+                    </div>
+                <ul className="checkout_items">
+                            {cartItems.map(item => (
+                                <li key={cartItems.id}>
+                                    <div className = "checkout_name">
+                                            {item.name}
+                                            </div>
+                                        <div className = "ItemCounte"> 
+                                        {item.count}
+                                    </div>
+                                </li>
+                            ))}
+
+                        </ul>
+
+                    <div className="checkout_total">
+                        <div >
+                            TOTAL{"   "}
+                            {formatCurrency(
+                                cartItems.reduce((a, c) => a + c.price * c.count, 0)
+                            )}
+                        </div>
+                    </div>
+                </div>
+        
                 <form onSubmit={this.createOrder}>
                     <ul className="form_container">
-                        <li>
+                        <li key="inputName">
                             <label>Name</label>
                             <br></br>
                                 <input
-                                name="Name"
+                                name="name"
                                 type="text"
                                 required
                                 onChange = {this.handleInput}
                                 ></input>
                         </li>
-                        <li>
+                        <li key="inputNum">
                             <label>Phone Number</label>
                             <br></br>
                                 <input
-                                name="Phone Number"
+                                name="phoneNumber"
                                 type="text"
                                 required
                                 onChange = {this.handleInput}
                                 ></input>
                         </li>
-                        <li>
+                        <li key="inputPeckupPoint">
                             <label>Peckup Point</label>
                             <br></br>
-                            <select>
+                            <select name="peckupPoint"
+                                    onChange = {this.handleInput}>
                                <option value="point1">Point1</option> 
                                <option value="point2">Point2</option> 
                                <option value="point3">Point3</option> 
                                <option value="point4">Point4</option> 
                             </select>
                             
+                        </li>
+                        <li key="checkOutBtn">
+                            <button  type="submit" className="checkout_btn"
+                            onSubmit={(event) => {
+                                this.props.createOrder(event)
+                            }}>
+                                Checkout
+                            </button>
+                            <button className="checkout_btn" onClick={() => {
+                            this.props.showCheckout();
+                            }}>
+                                Keep Buying
+                            </button>
                         </li>
                     </ul>
                 </form>
