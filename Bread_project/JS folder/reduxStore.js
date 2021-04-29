58433d7cf4c940e4eeeeac2055be97e826e93144
Nadav Yair <nadav.isshar@gmail.com>
2021-04-29 10:32:39 +0300

    Add files via upload

--- Bread_project/JS folder/reduxStore.js
+++ Bread_project/JS folder/reduxStore.js
@@ -1,12 +1,16 @@
-import { createStore, applyMiddleware,  combineReducers } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { shopItemsReducer } from './reducers/shopItemReducers';
 
 const initialState = {}
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 // create the redux store
-const reduxStore = createStore(
+const store = createStore(
     combineReducers({
-        shopItems: shopItemsReducer,
-    }),
+        ShopItems: shopItemsReducer,
+    }), 
     initialState, 
-    )
+    composeEnhancers(applyMiddleware(thunk))
+    );
+
+    export default store;
--- Bread_project/JS folder/shopItemActions.js
+++ Bread_project/JS folder/shopItemActions.js
@@ -1,10 +1,12 @@
 import { FETCH_SHOPITEMS } from "../types"
 
 
-export const fetchShopItems = ()  => async(dispatch) => {
-    const res = await fetch("/api//api/ShopItems");
+export const fetchShopItems = ()  => async (dispatch) => {
+    const res = await fetch("/api/ShopItems");
+    const data = res.json();
+    console.log(data);
     dispatch({
         type: FETCH_SHOPITEMS,
-        payload: res.data,
+        payload: data,
     });
 }
