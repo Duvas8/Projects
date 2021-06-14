116f233c30ae87d8ffa2f338f5d991274a762450
Nadav Yair <nadav.isshar@gmail.com>
2021-06-14 13:05:33 +0300

    Update ShopItems.js

--- Bread_project/JS folder/ShopItems.js
+++ Bread_project/JS folder/ShopItems.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
 import classes from '../ShopItem/ShopItem.module.css';
 import formatCurrency from '../../../util'
@@ -9,49 +9,51 @@ import { Zoom } from "react-awesome-reveal";
 import { connect} from 'react-redux';
 import { fetchShopItems } from '../../../actions/shopItemActions'
 
-class ShopItems extends Component {
-    constructor(props){
-        super(props) 
-            this.state = {
-                shopItem: null,
-            };
-    }
 
-    componentDidMount(){
-        this.props.fetchShopItems();
-    }
+function ShopItems (props){
 
-  // take the the shop item/product that the user click on and make the the shopItem equl to the shop item/product
-    openModal = (shopItem) => {
-        this.setState({shopItem})
-    }
-    // this fuc makes the modal unvisable, return it to the original state of null
-    closeModal = () => {
-        this.setState({shopItem: null})
+    const initialState = {
+        shopItem: null
     }
     
-render() {
+    const [item, setItem] = useState(initialState)
+
+    // convert componentdidmount to hooks if useEffect ends with empty array it do the same as componentdidmount
+    useEffect(() => {
+        props.fetchShopItems()
+      }, [])
+
+  // take the the shop item/product that the user click on and make the the shopItem equl to the shop item/product
+    const openModal = (shopItem) => {
+        setItem({shopItem})
+    }
+    // this fuc makes the modal unvisable, return it to the original state of null
+   const closeModal = () => {
+        setItem({shopItem: null})
+    }
+    
+
     // creates to shop item for the modal in the react render
-    const { shopItem } = this.state;
+    const { shopItem } = item;
 
     return (
     <Axu>
         <div className={classes.cart_Icon} onClick={() => {
-            this.props.openCart();
+            props.openCart();
         }}>
             <img alt='cart-icon' src='./Images/icons8-shopping-cart-32.png'></img>
         </div>
     <Fade direction='up' triggerOnce cascade duration="750">
-        { !this.props.ShopItems ? (
-        <div>Loading...</div>
+        { !props.ShopItems ? (
+        <div className={classes.loading}>Loading...</div>
         ) : (
         <ul className={classes.shopItem_container}>
-            { this.props.shopItems.map((shopItem ) => (
-                <li key={shopItem.id}>
+            { props.shopItems.map((shopItem , index ) => (
+                <li key={index}>
                     <div  className = {classes.ShopItem}>
                         <div className = {classes.Image}>
                             <a herf = {"#" + shopItem.id} 
-                            onClick={() => {this.openModal(shopItem)}}>
+                            onClick={() => {openModal(shopItem)}}>
                             <img src = {shopItem.image} alt="bread_image" ></img>
                             </a>
                         </div>
@@ -72,7 +74,7 @@ render() {
                         </div>
                             <p className = {classes.price} >{formatCurrency(shopItem.price)}</p>
                             <div className = {classes.amountControlContainer}>
-                                <button onClick={() => this.props.addToCart(shopItem)} className  = {classes.Btn} > Add To Cart</button>
+                                <button onClick={() => props.addToCart(shopItem)} className  = {classes.Btn} > Add To Cart</button>
                             </div>
                     </div>
                     </li>
@@ -84,11 +86,11 @@ render() {
   </Fade>
    {shopItem && (
         <Modal isOpen={true}
-        onRequestClose={this.closeModal}>
+        onRequestClose={closeModal()}>
             <Zoom>
-                <div>
+                <div className={classes.modal_container}>
                 
-                    <div className={classes.close_btn} onClick={() => {this.closeModal()}}>
+                    <div className={classes.close_btn} onClick={() => {closeModal()}}>
                         <Zoom delay="100">
                         <div className={classes.close_btn_bar1}></div>
                         <div className={classes.close_btn_bar2}></div>
@@ -119,7 +121,7 @@ render() {
                         </div>
                         <p className = {classes.price_modal} >{formatCurrency(shopItem.price)}</p>
                             <div className = {classes.amountControlContainer}>
-                                <button onClick={() => this.props.addToCart(shopItem)} className  = {classes.Btn} > Add To Cart</button>
+                                <button onClick={() => props.addToCart(shopItem)} className  = {classes.Btn} > Add To Cart</button>
                             </div>
                      </div>
                 </div>
@@ -128,36 +130,9 @@ render() {
     )}
     </Axu> 
        )
-  }
+  
 };
 // export the shop items and conets the redux to the component
 export default connect((state) => ({ShopItems: state.ShopItems.items}), 
 {fetchShopItems})
 (ShopItems);
-
-
-     /* <button onClick={shopItem.addItem} className  = {classes.Btn} > + </button>
-                    <div className = {classes.ItemCounte}>  </div>
-                    <button onClick={shopItem.subItem} className  = {classes.Btn} > - </button> 
-                    
-                    .Btn { 
-    display: flex;
-    color: burlywood;
-    justify-content: center;
-    align-items: center;
-    border-radius: 100%;
-    width: 50px;
-    height: 50px;
-    font-size: 2rem;
-    background-color: white;
-    border: none;
-    padding: 7%;
-    margin: 10%;
-    box-shadow:  1px 3px 6px darkgrey ;
-}<div className = {classes.ShopItem_containr}> </div>
-
-    /* */
-
-
-    // <h2>{shopItem.name}</h2>
-   // <p className = {classes.info} >{shopItem.info}</p>
