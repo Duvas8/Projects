43489eef40b9759a421dd1555436023cd17c2990
Nadav Yair <nadav.isshar@gmail.com>
2021-06-14 13:11:58 +0300

    Update server.js

--- Bread_project/JS folder/server.js
+++ Bread_project/JS folder/server.js
@@ -1,5 +1,3 @@
-
-
 const express  = require("express");
 const bodyParser  = require("body-parser");
 const mongoose  = require("mongoose");
@@ -9,15 +7,15 @@ const shortid = require('shortid');
 
 const app = express();
 app.use(bodyParser.json());
-
+// צריך לבדוק עוד על מונגו ואיך עובדים עם השרתים
 mongoose.connect("mongodb://localhost/bread_project_db", {
     useNewUrlParser:true,
     useCreateIndex: true,
     useUnifiedTopology:true,
 });
-
+// note: the server  name neeids to be Different  from the model name,  see the to lines under
 const ShopItem = mongoose.model(
-    "ShopItems",
+    "shopitems",
     new mongoose.Schema({
     _id: {  type: String, default: shortid.generate },
     name: String,
@@ -27,23 +25,54 @@ const ShopItem = mongoose.model(
     flourType: [String],
 }))
 
-app.get("/api/ShopItems", async (req, res)=>{
+app.get("/api/shopitems", async (req, res)=>{
 
     const ShopItems = await ShopItem.find({});
     res.send(ShopItems);
+
 });
 
-app.post("/api/ShopItems", async (req, res)=>{
+app.post("/api/shopitems", async (req, res)=>{
 
     const newShopItem = new  ShopItem(req.body);
     const savedShopItem = await newShopItem.save();
     res.send(savedShopItem);
 });
 
-app.delete("/api/ShopItems:id", async (req, res)=> {
+app.delete("/api/shopitems/:id", async (req, res) => {
     const deletedShopItem = await ShopItem.findByIdAndDelete(req.params.id);
     res.send(deletedShopItem);
-})
+  });
 
 const port = process.env.PORT || 5000;
-app.listen( port, () => console.log("server at http://localhost:5000"));
+app.listen( port, () => console.log(`server at http://localhost:${port}`));
+
+
+
+
+/*
+
+
+
+
+import express from "express";
+import data from "./data.js"
+
+
+
+const app = express();
+
+app.get('/api/ShopItems',  (req, res)=>{
+    
+     res.send(data.shopItems);
+});
+
+
+app.get('/',  (req, res)=>{
+    
+    res.send("server ready");
+});
+
+const port = process.env.PORT || 5000;
+app.listen( port, () => console.log(`server at http://localhost:${port}`));
+*/
