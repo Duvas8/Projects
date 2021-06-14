39d992e66c28b3505067033aa635b3570de6135c
Nadav Yair <nadav.isshar@gmail.com>
2021-06-14 13:04:54 +0300

    Update shopItemActions.js

--- Bread_project/JS folder/shopItemActions.js
+++ Bread_project/JS folder/shopItemActions.js
@@ -1,12 +1,11 @@
 import { FETCH_SHOPITEMS } from "../types"
 
-
-export const fetchShopItems = ()  => async (dispatch) => {
-    const res = await fetch("/api/ShopItems");
-    const data = res.json();
+export const fetchShopItems = () => async (dispatch) => {
+    const res = await fetch("/api/shopitems");
+    const data = await res.json();
     console.log(data);
     dispatch({
         type: FETCH_SHOPITEMS,
-        payload: data,
+        payload: data
     });
 }
