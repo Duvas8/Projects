089b312d15e56c3c3c236b931d9ff9e589ced228
Nadav Yair <nadav.isshar@gmail.com>
2021-06-16 13:34:09 +0300

    Update Filter.js

--- Bread_project/JS folder/Filter.js
+++ Bread_project/JS folder/Filter.js
@@ -7,15 +7,18 @@ export default class Filter extends Component {
         return (
             <div className={classes.Filter}>
                 <div className={classes.filterResult}>{this.props.count} Prodacts</div>
-                <div className={classes.filterSort}> Order{" "} 
-                <select value={this.props.sort} onChange={()=>this.props.sortItems}>
+                <div className={classes.filterSort}>
+                     Order{" "} 
+                <select value={this.props.sort} onChange={this.props.sortItems}>
                 <option value='Latest'>Latest</option>
                 <option value='Lowest'>Lowest To Highest</option>
                 <option value='Highest'>Highest To Lowest</option>
                     </select>
                     </div>
-                <div className='filterFlourType'> Flour Type{" "}
+                <div className='filterFlourType'> 
+                Flour Type{" "}
                 <select value={this.props.type} onChange={this.props.filterItems}>
+                    <option value=''>ALL</option>
                     <option value='Wheat'>Wheat</option>
                     <option value='Simplify'>Simplify</option>
                     <option value='Spelt'>Spelt</option>
