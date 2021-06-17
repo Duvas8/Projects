97438d234fd0fa2b4242019d30fdb8b79ae3028d
Nadav Yair <nadav.isshar@gmail.com>
2021-06-17 15:57:24 +0300

    Update shopItemActions.js

--- Bread_project/JS folder/shopItemActions.js
+++ Bread_project/JS folder/shopItemActions.js
@@ -1,5 +1,6 @@
 import { FETCH_SHOPITEMS } from "../types"
 import { FILTER_SHOPITEMS_BY_FLOUR_TYPE} from  "../types"
+import { ORDER_SHOPITEMS_BY_PRICE } from "../types"
 
 export const fetchShopItems = () => async (dispatch) => {
     const res = await fetch("/api/shopitems");
@@ -11,17 +12,39 @@ export const fetchShopItems = () => async (dispatch) => {
     });
 }
 
-export const filterShopItems = (shopItems , type) => (dispatch) => {
+export const filterShopItems = (shopItems , typeOfFlour) => (dispatch) => {
     dispatch({
         type: FILTER_SHOPITEMS_BY_FLOUR_TYPE,
         payload: {
-            type: type,
+            typeOfFlour: typeOfFlour,
                 items: 
-                type === "" 
+                typeOfFlour === "" 
                 ? shopItems
-                : shopItems.filter((x) => x.flourType.indsexOf(type) >= 0),
+                : shopItems.filter((x) => x.flourType.indsexOf(typeOfFlour) >= 0),
         },
     });
 };
 
-
+export const sortShopItems = (filterdItems, sort) => (dispatch) => {
+    const sortedItems =  filterdItems.slice()
+    if( sort === ""){
+        sortedItems.sort((a, b)  => (a._id > b._id ? 1 : -1));
+    } else { 
+        sortedItems.sort((a ,b) => (
+            sort === "Lowest"
+            ? a.price > b.price 
+                ? 1 
+                : -1
+            : a.price > b.price
+                ? -1
+                : 1
+            ))
+    }
+    dispatch({
+        type: ORDER_SHOPITEMS_BY_PRICE, 
+        payload: {
+            sort: sort,
+            items: sortedItems,
+        }, 
+    })
+}  
