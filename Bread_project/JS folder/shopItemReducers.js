d9769f93d9fc9bb14d7ef631702f378928ab14c8
Nadav Yair <nadav.isshar@gmail.com>
2021-06-23 11:51:29 +0300

    Update shopItemReducers.js

--- Bread_project/JS folder/shopItemReducers.js
+++ Bread_project/JS folder/shopItemReducers.js
@@ -1,6 +1,6 @@
-import { FETCH_SHOPITEMS } from "../types"
-import { FILTER_SHOPITEMS_BY_FLOUR_TYPE} from  "../types"
-import { ORDER_SHOPITEMS_BY_PRICE } from "../types"
+import { FETCH_SHOPITEMS, FILTER_SHOPITEMS_BY_FLOUR_TYPE, ORDER_SHOPITEMS_BY_PRICE} from "../types"
+
+
 
 
 // reduce function  that takes 2 parmeters state that equl to empty object and an action 
@@ -13,9 +13,16 @@ export const shopItemsReducer = (state = {}, action) => {
         typeOfFlour: action.payload.typeOfFlour,
         filteredItems: action.payload.items,
       };
+    case ORDER_SHOPITEMS_BY_PRICE:
+      return{
+        // note not to forget to use spred opertor one things that chainge
+        ...state,
+        sort: action.payload.sort,
+        filteredItems: action.payload.items,
+      };
     // if the action.type equl to FETCH_SHOPITEMS the switch uses the none default case 
    case FETCH_SHOPITEMS:
-        return {items: action.payload} ;  
+        return {items: action.payload, filteredItems: action.payload} ;  
     default:
         return state;
  }
