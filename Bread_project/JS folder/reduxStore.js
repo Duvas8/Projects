0741317a91288f21ec663edd566b7bdab6e80e7a
Nadav Yair <nadav.isshar@gmail.com>
2021-06-27 15:38:34 +0300

    Update reduxStore.js

--- Bread_project/JS folder/reduxStore.js
+++ Bread_project/JS folder/reduxStore.js
@@ -1,13 +1,15 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
+import { cartReducer } from './reducers/cartReducer';
 import { shopItemsReducer } from './reducers/shopItemReducers';
 
 const initialState = {}
-const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // create the redux store
 const store = createStore(
     combineReducers({
         ShopItems: shopItemsReducer,
+        cart:  cartReducer
     }), 
     initialState, 
     composeEnhancers(applyMiddleware(thunk))
