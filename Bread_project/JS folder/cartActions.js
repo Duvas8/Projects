5579befaf0a3a3844887c629a302b581b48d9ef9
Nadav Yair <nadav.isshar@gmail.com>
2021-07-06 17:50:15 +0300

    Update cartActions.js

--- Bread_project/JS folder/cartActions.js
+++ Bread_project/JS folder/cartActions.js
@@ -1,42 +1,77 @@
-import { ADD_TO_CART, REMOVE_FROM_CART } from "../types";
+// im am try to see if ican use product insted of ShopItem because it is much eseyer// make a copy of the items in the cart
+import { ADD_TO_CART, REMOVE_FROM_CART , INCREMENT , DECREMENT} from "../types";
 
-// im am try to see if ican use product insted of ShopItem because it is much eseyer
 export const addToCart = (product) => (dispatch, getState) => {
-    const cartItems = getState().cart.cartItems.slice(); // make a copy of the items in the cart
-    let alredyInCart = false;
+  const cartItems = getState().cart.cartItems.slice();
+  let alreadyExists = false;
+  cartItems.forEach((x) => {
+    if (x._id === product._id) {
+      alreadyExists = true;
+      x.count++;
+    }
+  });
+  if (!alreadyExists) {
+    cartItems.push({ ...product, count: 1 });
+  }
+  dispatch({
+    type: ADD_TO_CART,
+    payload: { cartItems },
+  });
+  localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  console.log(cartItems)
+};
+
+export const removeFromCart = (product) => (dispatch, getState) => {
+  const cartItems = getState().cart.cartItems.slice()
+    .filter((x) => x._id !== product._id);
+  dispatch({
+        type: REMOVE_FROM_CART,
+        payload: { cartItems } 
+    });
+  localStorage.setItem("cartItems", JSON.stringify(cartItems));
+};
+
+
+export const addAmount = (product) => (dispatch, getState) => {
+    const cartItems = getState().cart.cartItems.slice();
     cartItems.forEach((x) => {
         if(x._id === product._id){
-            alredyInCart = true;
-            x.count++;
-        }
-    })
-    if(!alredyInCart){
-        cartItems.push({...product, count: 1})
-    }
+            x.count++
+        }  
+    });
     dispatch({
-        type: ADD_TO_CART,
+        type: INCREMENT,
         payload: { cartItems },
-    })
-    localStorage.setItem("cartItems", JSON.stringify(cartItems))
+      });
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+    console.log(cartItems)
 };
 
-export const removeFromCart  = (product) => (dispatch ,getState ) => {
-    const cartItems = getState().cart.cartItems.slice()
-    .filter(
-        x => x._id !== product._id
-    );
+export const subAmount = (product, removeFromCart) => (dispatch, getState) => {
+    const cartItems = getState().cart.cartItems.slice();
+    cartItems.forEach((x) => {
+        if(x._id === product._id){
+            x.count--  
+        };
+    });
     dispatch({
-        type: REMOVE_FROM_CART, 
+        type: DECREMENT,
         payload: { cartItems },
-    })
-    localStorage.setItem("cartItems", JSON.stringify(cartItems))
-};
-
-
+      });
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+    console.log(cartItems)
+}
 
 /*this.setState({cartItems})
     console.log(cartItems);
     ;
+
+ if(x.count === 1){
+                getState().cart.cartItems.slice().filter((y) => y._id !== product._id);
+        }else{
+        
+        }
+
     addAmount = (shopItem) => {
             const cartItems = state.cartItems;
             cartItems.forEach((x) => {
@@ -60,5 +95,5 @@ export const removeFromCart  = (product) => (dispatch ,getState ) => {
                 } 
                 
             }) 
-        } need to implement  this in the this in  the redux store
-    */
+        } need to implement  this in the this in  the redux store */
+   
