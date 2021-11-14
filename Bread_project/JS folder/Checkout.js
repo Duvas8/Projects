25b5378d905f17874530111b47f0025141694c16
Nadav Yair <nadav.isshar@gmail.com>
2021-11-14 14:53:57 +0200

    Update Checkout.js

--- Bread_project/JS folder/Checkout.js
+++ Bread_project/JS folder/Checkout.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react'
 import  './Checkout.css';
 import { connect } from 'react-redux';
 import formatCurrency from '../../../util'
-import { Fade } from "react-awesome-reveal";
+import { Fade , Zoom} from "react-awesome-reveal";
+import { createOrder, clearOrder} from "../../../actions/orderActions"
+import Modal from 'react-modal';
+
 
 
 
@@ -13,32 +16,56 @@ class Checkout extends Component {
         this.state = {
             name: "",
             phoneNumber:"",
-            peckupPoint:""
+            peckupPoint:"",
          }
         };
 
-    handleInput = (e) => {
+
+        handleInput = (e) => {
         this.setState({[e.target.name]: e.target.value});
-        console.log(e.target.value);
        };
 
-       createOrder = (e) => {
+// need to add err hendel for phone number 
+        createOrder = (e) => {
         e.preventDefault();
         const order = {
             name: this.state.name,
             phoneNumber: this.state.phoneNumber,
             peckupPoint: this.state.peckupPoint,
-            cartItems: this.props.cartItems,
+            cartItems: JSON.parse(localStorage.getItem("cartItems")  || "[]" ),
+            total: this.props.cartItems.reduce((a, c) => a + c.price * c.count, 0 ),
         };
-       
-        this.props.createOrder(order)
+        
+        console.log(order);
+        if(isNaN(order.phoneNumber)){
+            alert("Phone Number need to be a number")
+            return 
+        }else {
+               this.props.createOrder(order)
+        }
+        
+    }
+
+
+    
+    closeModal = () => {
+        this.props.clearOrder();
+        this.props.showCheckout();
     };
+
+    openSucssesOrder = async (order , res) => {
+       if (order)
+        return res ;
+   }
 
     render() {
 
-        const {cartItems} = this.props;
-        return (
-            <div className="checkout_background">
+        const {cartItems , order} = this.props;
+        return (   
+             <div>
+            
+               { !order && ( 
+               <div className="checkout_background">
             <Fade  cascade duration="750">     
             <div className="checkout_container">
             <Fade  direction="up" cascade duration="750">
@@ -107,8 +134,9 @@ class Checkout extends Component {
                         </li>
                         <li key="checkOutBtn">
                             <button  type="submit" className="checkout_btn"
+                             
                             onSubmit={(event) => {
-                                this.props.createOrder(event)
+                                this.props.createOrder(event);
                             }}>
                                 Checkout
                             </button>
@@ -122,15 +150,78 @@ class Checkout extends Component {
                 </form>
                 </Fade>
             </div>
-            </Fade>
+            </Fade>  
+            </div>)}
+            { order && (
+            <Modal isOpen={true} onRequestClose={this.closeModal} >
+                <Zoom>
+                    
+                        <div className="close_btn" onClick={this.closeModal}> 
+                            <Zoom delay="100">
+                            <div className="close_btn_bar1"></div>
+                            <div className="close_btn_bar2"></div>
+                            </Zoom>
+                        </div>
+                     <div className="checkOut_modal">
+                        <div className="order_details">
+                        <h3 className="success_message">Your order has been placed.</h3>
+                        <h2>Order {order._id}</h2>
+                        <ul>
+                            <li>
+                                <div>Name:</div>
+                                <div>{order.name} </div>
+                            </li>
+                            <li>
+                                <div>Phone Number:</div>
+                                <div>{order.phoneNumber} </div>
+                            </li>
+                            <li>
+                                <div>Peckup Point:</div>
+                                <div>{order.peckupPoint} </div>
+                            </li>
+                            <li>
+                                <div>Date:</div>
+                                <div>{order.createdAt} </div>
+                            </li>
+                            <li>
+                            <div>Cart Items:</div>
+                            <div>
+                                {cartItems.map((x) => (
+                                <div>
+                                {x.count} {" x "} {x.name}
+                                </div>
+                                ))}
+                            </div>
+                            </li>
+                            <li>
+                                <div>Total:</div>
+                                <div>{order.total} </div>
+                            </li>
+                            <li>
+                                <div>PAY BOX LINK:</div>
+                                <link></link>
+                            </li>
+                        </ul>
+                        </div>
+                    </div>
+                    
+                </Zoom>
+            </Modal>
+                )}
+         
+
         </div>
         )
+        
     }
 }
 
 export default connect((state) => ({
+    order: state.order.order,
     cartItems: state.cart.cartItems,
  }),
- { }
+ { createOrder, clearOrder}
  )(Checkout);
 // try miking the checkout be on the shop component to be make it like login in some what
+/*}
+ */
