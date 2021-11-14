d5393359712f3d6c8d56e91d9e3bc7cccefe794c
Nadav Yair <nadav.isshar@gmail.com>
2021-11-14 14:54:31 +0200

    Update Layout.js

--- Bread_project/JS folder/Layout.js
+++ Bread_project/JS folder/Layout.js
@@ -1,21 +1,36 @@
 
 import React from 'react';
-
 import Aux from '../../hoc/Aux'
 import Hader from '../../containers/Hader/Hader'
 import MainPage from '../../containers/MainPage/MainPage'
+import Shop from '../Shop/shop'
+import './Layout.css'
+import Admin from '../Admin/Admin'
+import { BrowserRouter as Router, Switch , Route } from 'react-router-dom';
 
 
-
-const layout = (props) => (
-       <Aux >  
-           <Hader />
-           <MainPage />
-           <main>
-               
-               {props.children}
-            </main>
-       </Aux>
+const layout = () => (
+     
+           <Router>
+                <div className = "NavBar">
+                    <Hader />
+                </div>
+           
+                
+                    <Switch>
+                    <Aux>     
+                    <div  className = "MainContant">
+                        <Route path="/" exact component={MainPage} />
+                        <Route path="/Shop" component={Shop} />
+                    </div>
+                    <div className = "AdminPage">
+                    <Route path="/Admin" component={Admin} />
+                    </div>
+                     </Aux>
+                    </Switch>    
+                   
+            </Router>
+      
 );
     
          
